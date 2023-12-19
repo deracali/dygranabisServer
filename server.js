fe3ada3051b00de04ab4d054178b31f63cd055f3
deracali <chideracalistus00@gmail.com>
2023-12-18 22:44:57 -0800

    latest commit

--- server.js
+++ server.js
@@ -27,11 +27,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
-app.use(
-    cors({
-     origin: "https://admin.dygranabis.com/",
-    })
-   );
+app.use(cors())
 
 app.use((err, req, res, next) => {
     res.status(500).send({message:err.message})
