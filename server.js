2381510f0c0e5113699704b64545ee42eeb210f7
Samuel Calistus <89456479+deracali@users.noreply.github.com>
2024-02-12 14:04:54 -0800

    Update server.js

--- server.js
+++ server.js
@@ -32,35 +32,7 @@ app.use(cors())
 app.use((err, req, res, next) => {
     res.status(500).send({message:err.message})
 })
-function subscribe(req, res, next) {
-  let email = req.body.email; //Email entered
-  let dataCenter = 'us21';
-  let apiKey = '35e012270f2c8fd960a296d9873e5dae-us21';
-  let listID = '54d2faae35';
-  //Request settings
-  let options = {
-    url: `https://${dataCenter}.api.mailchimp.com/3.0/lists/${listID}/members`,
-    method: 'POST',
-    headers: { 'content-type': 'application/json', 'Authorization': `apikey ${apiKey}` },
-    body: JSON.stringify({ email_address: email, status: 'subscribed' })
-  }
-  // Make a simple POST call to MailChimp
-  request(options, function (error, response, body) {
-    try {
-      let respObj = {}; //Initial response object
-      if (response.statusCode === 200) {
-        respObj = { success: `Subscribed using ${email}!`, message: JSON.parse(response.body) };
-      } else {
-        respObj = { error: `Error trying to subscribe ${email}. Please try again.`, message: JSON.parse(response.body) };
-      }
-      res.send(respObj);
-    } catch (err) {
-      let respErrorObj = { error: 'There was an error with your request', message: err.message };
-      res.send(respErrorObj);
-    }
-  });
-  next();
-}
+
 app.use('/api/products',productRoutes)
 app.use('/api/users',userRouter)
 app.use('/api/blog',BlogRoutes)
