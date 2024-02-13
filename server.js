04f9401e445c260fda022a4f9d4bd44ba24de28c
Samuel Calistus <89456479+deracali@users.noreply.github.com>
2024-02-13 06:07:25 -0800

    Update server.js

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(cors())
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT", PATCH);
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT", "PATCH");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 })
