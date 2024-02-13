739c73f95e2edbf46753b75f0713081c4726718b
Samuel Calistus <89456479+deracali@users.noreply.github.com>
2024-02-13 06:57:43 -0800

    Update server.js

--- server.js
+++ server.js
@@ -25,15 +25,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
-const corsOptions = {
-  origin: 'http://localhost:5173',
-   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
-
-
+app.use(cors());
 
 app.use((err, req, res, next) => {
     res.status(500).send({message:err.message})
